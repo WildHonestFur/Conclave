25f3127c3adb383adfc9ea66a1e4263c6a8e21a6
WildHonestFur <137202612+WildHonestFur@users.noreply.github.com>
2025-07-26 10:28:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import cv2
+from PIL import ImageFont, ImageDraw, Image
+import numpy as np
+import mysql.connector as m
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+def get_top_5_teams():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="ssvm",
+        database="conclave"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, points FROM leader ORDER BY points DESC LIMIT 5")
+    teams = cursor.fetchall()
+    conn.close()
+    return teams
+
+@app.route('/')
+def leaderboard():
+    teams = get_top_5_teams()
+    teams_dict = [{"TeamName": t[0], "Score": t[1]} for t in teams]
+    return render_template('leaderboard.html', teams=teams_dict)
+
+if __name__ == "__main__":
+    app.run(debug=True)
